ead040eb9c46b5a69cea058292bddf66db2e917e
Fenotyp <martin.vanky@gmail.com>
2019-11-16 16:33:59 +0100

    Revamped the camera

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -1,41 +1,69 @@
 extends Camera
 
 var rot
-var ROTATESPEED = 0.005
-var MOVESPEED = 0.2
-var MOVESPEEDSPRINT = 5
+var ROTATESPEED = 0.1
+var MOVESPEED = 10
+var INITIALMOVESPEED
+var SPRINTSPEED = 5
 
+var forward = false
+var backward = false
+var right = false
+var left = false
+
+func _ready():
+	INITIALMOVESPEED = MOVESPEED
+
+func _process(delta):
+	if forward:
+		global_translate(-global_transform.basis.z * MOVESPEED  * delta)
+	if backward:
+		global_translate(global_transform.basis.z * MOVESPEED  * delta)
+	if right:
+		global_translate(global_transform.basis.x * MOVESPEED  * delta)
+	if left:
+		global_translate(-global_transform.basis.x * MOVESPEED  * delta)
 
 func _input(event):
-	
 	# Rotate the camera
 	if Input.is_action_pressed("camera_rotate"):
 		if event is InputEventMouseMotion:
-			rotation.x = (rotation.x - event.relative.y * ROTATESPEED)
-			rotation.y = (rotation.y - event.relative.x * ROTATESPEED)
+			rotation.x = (rotation.x - event.relative.y * ROTATESPEED  * get_process_delta_time())
+			rotation.y = (rotation.y - event.relative.x * ROTATESPEED  * get_process_delta_time())
 		
 		# Move the camera
 		if event is InputEventKey:
 			if Input.is_action_pressed("camera_move_forward"):
-				if Input.is_action_pressed("sprint"):
-					global_translate(-global_transform.basis.z * MOVESPEED * MOVESPEEDSPRINT)
-				else:
-					global_translate(-global_transform.basis.z * MOVESPEED)
-					
+				forward = true
+			if Input.is_action_just_released("camera_move_forward"):
+				forward = false
+				
 			if Input.is_action_pressed("camera_move_backward"):
-				if Input.is_action_pressed("sprint"):
-					global_translate(global_transform.basis.z * MOVESPEED * MOVESPEEDSPRINT)
-				else:
-					global_translate(global_transform.basis.z * MOVESPEED)
-
+				backward = true
+			if Input.is_action_just_released("camera_move_backward"):
+				backward = false
+				
 			if Input.is_action_pressed("camera_move_right"):
-				if Input.is_action_pressed("sprint"):
-					global_translate(global_transform.basis.x * MOVESPEED * MOVESPEEDSPRINT)
-				else:
-					global_translate(global_transform.basis.x * MOVESPEED)
-					
+				right = true
+			if Input.is_action_just_released("camera_move_right"):
+				right = false
+				
 			if Input.is_action_pressed("camera_move_left"):
-				if Input.is_action_pressed("sprint"):
-					global_translate(-global_transform.basis.x * MOVESPEED * MOVESPEEDSPRINT)
-				else:
-					global_translate(-global_transform.basis.x * MOVESPEED)
+				left = true
+			if Input.is_action_just_released("camera_move_left"):
+				left = false
+
+			if Input.is_action_just_pressed("sprint"):
+				MOVESPEED *= SPRINTSPEED
+			if Input.is_action_just_released("sprint"):
+				MOVESPEED = INITIALMOVESPEED
+	else:
+		ResetValues()
+
+
+func ResetValues():
+	MOVESPEED = INITIALMOVESPEED
+	forward = false
+	backward = false
+	right = false
+	left = false
---
+++ scripts/GameMaster.gd
@@ -0,0 +1,14 @@
+extends Node
+
+export (PackedScene) var ground
+export var MAPSIZE = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#var block = ground.instance()
+	#add_child(block)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
