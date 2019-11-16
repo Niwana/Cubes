11b13f249eca310168c4293a845a678bd3f0ce43
samuel wilhelmsson <samuel.wilhelmsson@gmail.com>
2019-11-16 11:20:16 +0100

    Basic setup

---
+++ KinematicBody.gd
@@ -0,0 +1,15 @@
+extends KinematicBody
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var hearingRadius = get_
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("hola amigo")
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	pass
